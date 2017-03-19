6889797b7bb9ef5f30c3c5302655f1ea628e5218
Shovon Hasan <im.shovonhasan@gmail.com>
2017-03-19 04:00:24 +0000

    Write JS functions to simulate rolling the bones and add Vue.js button to allow users to do so

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'rolls#index'
+  
+  resources :rolls do
+    collection do
+      get :reroll
+    end
+  end
 end
